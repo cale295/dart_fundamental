5becf0ca43f744bd2119513b329dc9f75b16c985
vargas <vargascahyadi@gmail.com>
2025-09-26 11:06:40 +0700

    commit kedua

--- bin/dart_fundamental.dart
+++ bin/dart_fundamental.dart
@@ -1,5 +1,8 @@
 import 'package:dart_fundamental/dart_fundamental.dart' as dart_fundamental;
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_fundamental.calculate()}!');
+void main(){
+  print("Hello world");
+  
+  String namaLengkap = "Vargas";
+  print(namaLengkap);
 }
